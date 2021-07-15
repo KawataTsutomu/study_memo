a1ab080945d8d3bd797832940821b187acd003bc
kawatatsutomu <tomtom.net1@gmail.com>
2021-07-16 08:45:10 +0900

    Googleアカウントでログイン可能に変更

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Auth::routes();
 // Googleでログイン
 Route::prefix('login')->name('login.')->group(function () {
     Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
 });
 // 記事一覧表示
 Route::get('/', 'ArticleController@index')->name('articles.index');
