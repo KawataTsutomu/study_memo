422545bdbc6ce1ae22c35b81e8234882a208676b
kawatatsutomu <tomtom.net1@gmail.com>
2021-07-19 11:13:04 +0900

    procfile作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
             <h1 class="text-center">
-                <a class="text-dark" href="/">memo</a>
+                <a class="text-dark" href="/">studymemo</a>
             </h1>
             <div class="card mt-3">
                 <div class="card-body text-center">
